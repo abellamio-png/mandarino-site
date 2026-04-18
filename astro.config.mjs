fa164026368e53a8d1aea07398c14e8adda23917
Andrea Bellamio <a.bellamio@mandarino.agency>
2026-04-18 16:41:57 +0200

    fix(build): installato @astrojs/cloudflare per abilitare il deploy di Keystatic SSR su Cloudflare Pages. Aggiunti loghi sulle card lavori

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  adapter: cloudflare(),
   integrations: [
     mdx(),
     react(),
